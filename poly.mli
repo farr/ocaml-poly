91f4e71be948c305d57b92a26e1e74da5ffc0c5d
Will M. Farr <w-farr@northwestern.edu>
2010-01-09 17:06:39 -0600

    Added GPL v3 to poly.ml{i}.

--- poly.mli
+++ poly.mli
@@ -1,3 +1,19 @@
+(*  poly.mli: Interface file for code to manipulate polynomials. 
+    Copyright (C) 2010 Will M. Farr <wmfarr@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. *)
+
 (** Evaluate and manipulate polynomials via their coefficients.  The
     convention is that the [i]th element of a coefficient array, [p],
     is the coefficient of x^[i]. *)
